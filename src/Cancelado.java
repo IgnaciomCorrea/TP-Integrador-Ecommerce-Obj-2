744b16852f9a53a36e13fd5d26f87deee3550f4a
TomasDominguez10 <tomaselias.dw@gmail.com>
2026-07-01 01:35:55 -0300

    Mensajes de execpciones

--- src/Cancelado.java
+++ src/Cancelado.java
@@ -7,26 +7,26 @@ public class Cancelado implements EstadoPedido {
     }
     @Override
     public void quitarVendible(Pedido pedido, Vendible vendible) {
-        throw new ExcepcionGeneral("Pedido cancelado, no se puede modificar");
+        throw new ExcepcionGeneral("El pedido ha sido cancelado , no se modificar el stock ");
     }
     @Override
     public void confirmar(Pedido pedido) {
-        throw new ExcepcionGeneral("No se puede confirmar un pedido cancelado");
+        throw new ExcepcionGeneral("No se  debe  confirmar un pedido cancelado");
     }
     @Override
     public void cancelar(Pedido pedido) {
-        throw new ExcepcionGeneral("El pedido ya está cancelado");
+        throw new ExcepcionGeneral("El pedido ya está cancelado , no se entregara ");
     }
     @Override
     public void pasarAEnPreparacion(Pedido pedido) {
-        throw new ExcepcionGeneral("No se puede reanudar un pedido cancelado");
+        throw new ExcepcionGeneral("No se puede reanudar un pedido cancelado ");
     }
     @Override
     public void pasarAEnviado(Pedido pedido) {
-        throw new ExcepcionGeneral("No se puede reanudar un pedido cancelado");
+        throw new ExcepcionGeneral("No se puede preparar  un pedido que ya esta cancelado ");
     }
     @Override
     public void pasarAEntregado(Pedido pedido) {
-        throw new ExcepcionGeneral("No se puede reanudar un pedido cancelado");
+        throw new ExcepcionGeneral("No se puede enviar un pedido que ya este cancelado ");
     }
 }
