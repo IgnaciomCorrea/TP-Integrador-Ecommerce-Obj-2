91f518779e155423a083360a171a148a80446517
TomasDominguez10 <tomaselias.dw@gmail.com>
2026-07-01 02:02:19 -0300

    cambios

--- src/Enviado.java
+++ src/Enviado.java
@@ -3,12 +3,12 @@ import exceptions.ExcepcionGeneral;
 public class Enviado implements EstadoPedido {
     @Override
     public void agregarVendible(Pedido pedido, Vendible vendible) {
-        throw new ExcepcionGeneral("No se puede modificar un pedido enviado");
+        throw new ExcepcionGeneral("No se puede agregar productos a  un pedido enviado");
     }
 
     @Override
     public void quitarVendible(Pedido pedido, Vendible vendible) {
-        throw new ExcepcionGeneral("No se puede modificar un pedido enviado");
+        throw new ExcepcionGeneral("No se puede quitar productos a  un pedido que esta  enviado");
     }
 
     @Override
@@ -27,12 +27,12 @@ public class Enviado implements EstadoPedido {
 
     @Override
     public void pasarAEnPreparacion(Pedido pedido) {
-        throw new ExcepcionGeneral("No se puede volver a preparación desde enviado");
+        throw new ExcepcionGeneral("No se puede volver a preparación desde el estado actual de  enviado");
     }
 
     @Override
     public void pasarAEnviado(Pedido pedido) {
-        throw new ExcepcionGeneral("El pedido ya está en enviado");
+        throw new ExcepcionGeneral("El pedido ya se encuentra en  enviado");
     }
 
     @Override
