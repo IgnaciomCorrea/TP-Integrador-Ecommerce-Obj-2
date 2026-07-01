da44e00fd7544456bdcce5e52e134f41b24800b4
TomasDominguez10 <tomaselias.dw@gmail.com>
2026-07-01 19:08:17 -0300

    cambios

---
+++ src/MetodoDeEnvio/CorreoArgentina.java
@@ -0,0 +1,12 @@
+package MetodoDeEnvio;
+
+public class CorreoArgentina {
+
+    /**
+     * Simula la librería provista por la cátedra.
+     */
+    public float estimarEnvio(float peso, Direccion direccionEnvio) {
+        return 0;
+    }
+
+}
---
+++ src/MetodoDeEnvio/Direccion.java
@@ -0,0 +1,5 @@
+package MetodoDeEnvio;
+
+public class Direccion {
+
+}
